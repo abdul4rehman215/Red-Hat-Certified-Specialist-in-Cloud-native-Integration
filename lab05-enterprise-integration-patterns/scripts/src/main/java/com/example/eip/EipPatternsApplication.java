78428a0e282cbbc41328dfcf022fab40a1a7058d
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-27 20:22:27 +0530

    Create EipPatternsApplication.java

---
+++ lab05-enterprise-integration-patterns/scripts/src/main/java/com/example/eip/EipPatternsApplication.java
@@ -0,0 +1,11 @@
+package com.alnafi.eip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EipPatternsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EipPatternsApplication.class, args);
+    }
+}
