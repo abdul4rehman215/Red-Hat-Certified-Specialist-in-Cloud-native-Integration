17d703d16667cf21541f6fdea31bed33f554dc34
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-27 19:57:10 +0530

    Create ApiGatewayApplication.java

---
+++ lab15-implementing-api-gateway-pattern-with-camel/scripts/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+// src/main/java/com/example/gateway/ApiGatewayApplication.java
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApiGatewayApplication.class, args);
+  }
+}
