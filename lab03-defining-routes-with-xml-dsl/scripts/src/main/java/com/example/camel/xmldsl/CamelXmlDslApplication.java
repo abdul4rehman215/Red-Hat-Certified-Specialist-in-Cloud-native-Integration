835fbc2dbc5cc8ac74374cedcd4f99e3e0a0803c
Abdul Rehman <abdul4rehman215@gmail.com>
2026-02-27 17:22:58 +0530

    Create CamelXmlDslApplication.java

---
+++ lab03-defining-routes-with-xml-dsl/scripts/src/main/java/com/example/camel/xmldsl/CamelXmlDslApplication.java
@@ -0,0 +1,11 @@
+package com.alnafi.camel.xmldsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelXmlDslApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CamelXmlDslApplication.class, args);
+    }
+}
